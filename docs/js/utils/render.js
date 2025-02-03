f2a5397a49ca3c3b10963ebbc105f57b1d03480d
ckdvk <javicemarpe@gmail.com>
2025-02-03 22:02:20 +0800

    .

--- docs/js/utils/render.js
+++ docs/js/utils/render.js
@@ -13,8 +13,7 @@ const lettersAccentedDict = {
     'I': 'Í',
     'O': 'Ó',
     'U': 'Ú',
-    // 'n': 'ñ',
-    // 'N': 'Ñ'
+
 }
 
 const strangeLettersAccentedDict = {
@@ -32,9 +31,7 @@ const strangeLettersAccentedDict = {
 
 
 export function normalize(text) {
-    // removes accents, {}, etc
-    // text: string
-    // returns: string
+
 
     var normalizedText = text
     normalizedText = normalizedText.replaceAll('{', '').replaceAll('}', '')
@@ -51,16 +48,6 @@ export function normalize(text) {
 }
 
 export function renderResearchInfo(researchItem) {
-
-    // renders the research item in the html page
-    // researchItem: a dictionary with keys among the following: name, year, author, title, and journal 
-    // returns a string with the html code to be inserted in the html page. Usual structure of a research item:
-    // Ritva Hurri-Syrj\"anen et al. “On the BBM-
-    // phenomenon in fractional Poincar\'e-Sobolev in-
-    // equalities with weights”. In: Interna-
-    // tional Mathematics Research Notices (Sept. 2022)
-
-    // normalize: remove accents, {}, etc:
 
 
     for (var key in researchItem) {
--- docs/js/utils/togglestyle.js
+++ docs/js/utils/togglestyle.js
@@ -1,5 +1,4 @@
 
-// This function toggles the light-theme class on the body element. This class is used to change the theme of the page. The class is defined in the docs\css\style.css file. it will be called in the index.html file, when the user clicks on the toggle button.
 
 
 
